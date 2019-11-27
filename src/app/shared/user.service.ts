5b26698f8cf92b4afcceb6af6ef15a14c2897ab9
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2019-11-27 16:34:55 +0000

    add form validation for signup; add styling

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -9,7 +9,7 @@ export class UserService {
   constructor(private formBuilder: FormBuilder) { }
 
   formModel = this.formBuilder.group({
-    Email: ['', Validators.required],
-    Password: ['', Validators.required, Validators.minLength(8)]
-  })
+    Email: ['', [Validators.required, Validators.email]],
+    Password: ['', [Validators.required, Validators.minLength(8)]]
+  });
 }
