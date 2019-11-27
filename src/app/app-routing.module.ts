71cb4ad6a15ee6abdd10e06bc4bbdc71d1bb01f0
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2019-11-27 14:59:33 +0000

    configured user routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { SignupComponent } from './user/signup/signup.component';
+import { LoginComponent } from './user/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/user/login', pathMatch: 'full'},
+  {path: 'user', component: UserComponent,
+  children: [
+    {path: 'signup', component: SignupComponent}, //user/signup
+    {path: 'login', component: LoginComponent}    //user/login
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
