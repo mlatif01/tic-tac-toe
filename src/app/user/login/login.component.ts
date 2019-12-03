713cc54503740fb5b461616d093289801d63b7b5
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2019-12-03 13:24:56 +0000

    added form reset

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/shared/user.service';
-import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -19,6 +18,9 @@ export class LoginComponent implements OnInit {
     if (this.service.formModel.value.Email == "milad@latif.com" && this.service.formModel.value.Password == "miladlatif") {
       this.router.navigateByUrl('/home');
     }
+    this.service.formModel.reset();
+    console.log("SHOULD RESET");
+
   }
 
 }
